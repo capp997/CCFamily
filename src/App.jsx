17753c11667bb0886b5e1054cf9a026fc1b744cc
capp997 <63300856+capp997@users.noreply.github.com>
2025-08-28 23:24:09 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
-import ThankYou from "./pages/ThankYou";
-import { useEffect } from "react";
 import { initGA } from "./analytics";
-import React from "react";
 
 export default function App() {
   useEffect(() => {
-    initGA();
+    initGA("TU_GOOGLE_ANALYTICS_ID"); // reemplaza con tu ID
   }, []);
 
   return (
-    <BrowserRouter>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Mi Sitio
-          </Typography>
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/contact">Contact</Button>
-          <Button color="inherit" component={Link} to="/projects">Projects</Button>
-          <Button color="inherit" component={Link} to="/dashboard">Dashboard</Button>
-        </Toolbar>
-      </AppBar>
-
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/thank-you" element={<ThankYou />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
