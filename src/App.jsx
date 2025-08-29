eeffeb882f9db28c7eb2c3b59b6aa27954245fc6
capp997 <63300856+capp997@users.noreply.github.com>
2025-08-28 23:42:26 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
 import { initGA } from "./analytics";
 
 export default function App() {
@@ -13,10 +14,11 @@ export default function App() {
 
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
