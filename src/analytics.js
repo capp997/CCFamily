d2a3ac4649315dfc9f4fef9624e509310d7d4938
capp997 <63300856+capp997@users.noreply.github.com>
2025-08-28 23:35:36 -0500

    Update analytics.js

--- src/analytics.js
+++ src/analytics.js
@@ -1,14 +1,9 @@
-export const initGA = (trackingId) => {
-  if (!window.gtag) {
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
-    document.head.appendChild(script);
-
+export const initGA = (GA_MEASUREMENT_ID) => {
+  if (window && !window.gtagInitialized) {
     window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
-    window.gtag = gtag;
-    gtag("js", new Date());
-    gtag("config", trackingId);
+    function gtag() { window.dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', GA_MEASUREMENT_ID);
+    window.gtagInitialized = true;
   }
 };
