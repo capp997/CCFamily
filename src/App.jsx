e90df82ab204ff793eb28aae8cbe0badc546f665
capp997 <63300856+capp997@users.noreply.github.com>
2025-08-28 21:45:59 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import { useEffect } from "react";
+import { initGA } from "./analytics";
+
+export default function App() {
+  useEffect(() => {
+    initGA();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Mi Sitio
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/contact">Contact</Button>
+          <Button color="inherit" component={Link} to="/projects">Projects</Button>
+          <Button color="inherit" component={Link} to="/dashboard">Dashboard</Button>
+        </Toolbar>
+      </AppBar>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
