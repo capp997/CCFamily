5ed4a65777719429d8d97fd1b79a79f4a4cec5b5
capp997 <63300856+capp997@users.noreply.github.com>
2025-08-28 21:42:36 -0500

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Typography, Container } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Container>
+      <Typography variant="h3" gutterBottom>
+        Bienvenido a la Home
+      </Typography>
+      <Typography>
+        Esta es la página principal de tu sitio web con Material UI.
+      </Typography>
+    </Container>
+  );
+}
